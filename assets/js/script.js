16ff0532bf3ee43a70c7dfd923201865d835a351
Anjalee Kulasinghe <anjaleek@gmail.com>
2023-08-19 21:29:06 +0000

    add favicon and add finally tidy ups for the users

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,6 +15,14 @@ document.addEventListener("DOMContentLoaded", function() {
         });
     }
 
+    document.getElementById("answer-box").addEventListener("keydown", function(event){
+
+        if(event.key === "Enter") {
+            checkAnswer();
+        }
+
+    });
+
     runGame("addition");
 
 });
